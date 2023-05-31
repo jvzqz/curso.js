c6a5c1aff737a52da281cae11fb94631f7e8ef95
Julia Eva <julia.vazquez289@gmail.com>
2023-05-31 20:15:15 -0300

    clase y array

--- file.js
+++ file.js
@@ -1,5 +1,35 @@
 console.log ("Calculá tu préstamo en simples pasos")
 
+confirm("¿Es usted cliente del banco?") 
+
+let nombre = prompt("Indicá tu nombre:");
+let cuil = prompt ("Indicá tu CUIL:");
+let mail= prompt ("Indicá tu mail:");
+let lista_cliente = [];
+
+class cliente_banco {
+    constructor (nombre, cuil, mail){
+        this.nombre = nombre;
+        this.cuil = cuil;
+        this.mail = mail;
+    }
+
+    saludar(){
+        console.log ("Bienvenido al simulador de préstamos del banco Ciudad",this.nombre);
+    }
+      
+    get_cliente(){
+        console.log ("Nombre:",this.nombre);
+        console.log ("Cuil:",this.cuil);
+        console.log ("Mail:",this.mail);
+    }   
+}
+
+let cliente1 = new cliente_banco (nombre, cuil, mail);
+cliente1.saludar()
+cliente1.get_cliente()
+lista_cliente.push(cliente1);
+
 while (confirm("¿Desea hacer un cálculo de préstamo?")) {
 
 let monto = parseFloat (prompt ("Ingrese el monto del préstamo:"))
@@ -9,9 +39,7 @@ let prestamo = simular_prestamo (monto,cuotas);
 console.log("Monto Inicial: ", monto);
 console.log("Cuotas: ", cuotas);
 console.log("Monto final: ", prestamo);
-
 }
-
 
 function simular_prestamo (monto, cuotas) {
 
